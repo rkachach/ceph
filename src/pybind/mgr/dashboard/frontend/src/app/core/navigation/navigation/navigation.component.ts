88d839082e82e6fae11b94fa30cc77d6083c24a3
Afreen Misbah <afreen@ibm.com>
2025-12-16 19:39:32 +0530

    mgr/dashboard: Added header action for tasks and notifications
    Resolves: ISCE-1816
    
    - removed the custom css and using carbon component
    - fixed unit tests of notification page
    
    Signed-off-by: Afreen Misbah <afreen@ibm.com>
    (cherry picked from commit 68226642963c26b37ccc93055015e105f9d71d60)
    (cherry picked from commit c309ac3ce57fba8b007f50dd9a537c0d01798895)

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation/navigation.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation/navigation.component.ts
@@ -33,7 +33,7 @@ export class NavigationComponent implements OnInit, OnDestroy {
   clusterTokenStatus: object = {};
   summaryData: any;
 
-  rightSidebarOpen = false; // rightSidebar only opens when width is less than 768px
+  isNotifPanelOpen = false;
   showMenuSidebar = true;
 
   simplebar = {
@@ -145,8 +145,8 @@ export class NavigationComponent implements OnInit, OnDestroy {
     this.displayedSubMenu[menu] = !this.displayedSubMenu[menu];
   }
 
-  toggleRightSidebar() {
-    this.rightSidebarOpen = !this.rightSidebarOpen;
+  toggleSidebar() {
+    this.isNotifPanelOpen = !this.isNotifPanelOpen;
   }
 
   onClusterSelection(value: object) {
@@ -194,9 +194,7 @@ export class NavigationComponent implements OnInit, OnDestroy {
       }
     );
   }
-  toggleSidebar() {
-    this.notificationService.toggleSidebar(true, true);
-  }
+
   trackByFn(item: any) {
     return item;
   }
