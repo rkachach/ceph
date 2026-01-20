1d35bf87b68cf6da9019257d59826ed0a7f58861
Devika Babrekar <devika.babrekar@ibm.com>
2026-01-20 11:46:33 +0530

    mgr/dashboard: Generic Performace Chart - Carbon
    Resolves: ISCE-2803
    
    Fixes: https://tracker.ceph.com/issues/74396
    Signed-off-by: Devika Babrekar <devika.babrekar@ibm.com>
    
    fix performance charts
    
    mgr/dashboard: Generic Performance Chart - Area Chart Integration
    Fixes:https://tracker.ceph.com/issues/74396
    Signed-off-by: Devika Babrekar <devika.babrekar@ibm.com>
    
    add storage type view
    
    mgr/dashboard:Performance Charts - alignment adjustments
    fixes:https://tracker.ceph.com/issues/74396
    Signed-off-by: Devika Babrekar <devika.babrekar@ibm.com>
    
    Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
    (cherry picked from commit 1a6ebae1e33c1d556390eea86ec852525e44d690)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-overview-dashboard/rgw-overview-dashboard.component.ts

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard-v3/dashboard/dashboard-v3.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard-v3/dashboard/dashboard-v3.component.ts
@@ -88,7 +88,7 @@ export class DashboardV3Component extends PrometheusListHelper implements OnInit
   alertType: string;
   alertClass = AlertClass;
 
-  queriesResults: { [key: string]: [] } = {
+  queriesResults: Record<string, [number, string][]> = {
     USEDCAPACITY: [],
     IPS: [],
     OPS: [],
@@ -209,11 +209,12 @@ export class DashboardV3Component extends PrometheusListHelper implements OnInit
   }
 
   public getPrometheusData(selectedTime: any) {
-    this.queriesResults = this.prometheusService.getRangeQueriesData(
-      selectedTime,
-      UtilizationCardQueries,
-      this.queriesResults
-    );
+    this.prometheusService
+      .getRangeQueriesData(selectedTime, UtilizationCardQueries, true)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((results) => {
+        this.queriesResults = results;
+      });
   }
 
   getCapacityQueryValues(data: PromqlGuageMetric['result']) {
