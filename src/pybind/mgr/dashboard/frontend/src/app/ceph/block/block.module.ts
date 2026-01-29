8323d825cc0038fd695b562d76f12f0eb78f28b2
Afreen Misbah <afreen@ibm.com>
2026-01-30 05:15:53 +0530

    mgr/dashboard: Step three subsystem creation form
    Fixes: ISCE-2107
    
    - added step3 component
    - can add subsystem dhchap key
    - adding hosts keys is penidng due to step two PR
    
    Fixes https://tracker.ceph.com/issues/74095
    
    Signed-off-by: Afreen Misbah <afreen@ibm.com>
    (cherry picked from commit 87665b7aa06271f9fbf9229abf1f5ed0e3063d1a)
    (cherry picked from commit 8ee223ca183bf4bd0b9bf3741dbee02557511599)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/block.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/block.module.ts
@@ -51,6 +51,7 @@ import { environment } from '~/environments/environment';
 
 import { NvmeofGatewayGroupComponent } from './nvmeof-gateway-group/nvmeof-gateway-group.component';
 import { NvmeofSubsystemsStepOneComponent } from './nvmeof-subsystems-form/nvmeof-subsystem-step-1/nvmeof-subsystem-step-1.component';
+import { NvmeofSubsystemsStepThreeComponent } from './nvmeof-subsystems-form/nvmeof-subsystem-step-3/nvmeof-subsystem-step-3.component';
 import { NvmeofGatewayNodeComponent } from './nvmeof-gateway-node/nvmeof-gateway-node.component';
 import { NvmeofGroupFormComponent } from './nvmeof-group-form/nvmeof-group-form.component';
 import {
@@ -69,7 +70,8 @@ import {
   UIShellModule,
   TreeviewModule,
   TabsModule,
-  TagModule
+  TagModule,
+  LayerModule
 } from 'carbon-components-angular';
 
 // Icons
@@ -106,7 +108,8 @@ import Reset from '@carbon/icons/es/reset/32';
     ComboBoxModule,
     TabsModule,
     TagModule,
-    GridModule
+    GridModule,
+    LayerModule
   ],
   declarations: [
     RbdListComponent,
@@ -146,7 +149,8 @@ import Reset from '@carbon/icons/es/reset/32';
     NvmeofInitiatorsFormComponent,
     NvmeofGatewayNodeComponent,
     NvmeofGroupFormComponent,
-    NvmeofSubsystemsStepOneComponent
+    NvmeofSubsystemsStepOneComponent,
+    NvmeofSubsystemsStepThreeComponent
   ],
   exports: [RbdConfigurationListComponent, RbdConfigurationFormComponent]
 })
