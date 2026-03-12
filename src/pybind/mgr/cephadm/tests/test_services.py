ea1a1e5ef3af2824443c00142ed8cb67898ce640
John Mulligan <jmulligan@redhat.com>
2026-03-12 18:42:59 -0400

    mgr/cephadm: add unit test to check smb service get_dependencies call
    Fixes: https://tracker.ceph.com/issues/77762
    Resolves:IBMCEPH-16286
    
    Unmerged paths:
      (use "git add/rm <file>..." as appropriate to mark resolution)
            deleted by us:   tests/services/test_smb.py
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    Signed-off-by: Shweta Sodani <Shweta.Sodani@ibm.com>
    (cherry picked from commit e9dbee4f8be4e0866e3994efed2e82df5c107228)

--- src/pybind/mgr/cephadm/tests/test_services.py
+++ src/pybind/mgr/cephadm/tests/test_services.py
@@ -15,7 +15,7 @@ from cephadm.services.cephadmservice import MonService, CephadmDaemonDeploySpec
 from cephadm.services.iscsi import IscsiService
 from cephadm.services.nvmeof import NvmeofService
 from cephadm.services.monitoring import GrafanaService, AlertmanagerService, PrometheusService
-from cephadm.services.smb import SMBSpec
+from cephadm.services.smb import SMBSpec, SMBExternalCephCluster
 from cephadm.module import CephadmOrchestrator
 from ceph.deployment.service_spec import (
     AlertManagerSpec,
@@ -5019,6 +5019,38 @@ class TestSMB:
                 assert files.get('remote_control.ssl.key') == ceph_generated_key
                 assert files.get('remote_control.ca.crt') == cephadm_root_ca
 
+
+def test_smb_get_dependencies(cephadm_module):
+    from cephadm.services.smb import SMBService
+
+    spec = SMBSpec(
+        cluster_id='foxtrot',
+        features=['domain'],
+        config_uri='rados://.smb/foxtrot/config2.json',
+        join_sources=[
+            'rados:mon-config-key:smb/config/foxtrot/join2.json',
+        ],
+        include_ceph_users=[
+            'client.smb.fs.cephfs.share1',
+            'client.smb.fs.cephfs.share2',
+            'client.smb.fs.fs2.share3',
+        ],
+        ceph_cluster_configs=[
+            SMBExternalCephCluster(
+                alias="exo",
+                fsid='cf05db31-3d4e-4ffd-85ad-753434d5add1',
+                mon_host='[v2:192.168.76.200:3300/0,v1:192.168.76.200:6789/0]',
+                user='client.smb.remote1',
+                key='AQBAAK9ptYayIRAAQ1Bcpti9yMvX2Gl0KMmc4Q=='
+            )
+        ]
+    )
+
+
+    deps = SMBService.get_dependencies(cephadm_module, spec, spec.service_type)
+    assert deps == [
+        'smb+meta:ceph_cluster_config.exo=sha256:859b001f76df4d184b858b9c3e323ca8ff85a311414d0405f4484d17aa481ef3',
+    ]
 
 
 class TestMgmtGateway:
