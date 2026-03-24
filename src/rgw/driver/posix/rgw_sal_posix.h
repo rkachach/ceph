0d707a5575e0d3cc7dc95cbcd6b8a6fc0a962b17
Nithya Balachandran <nithya.balachandran@ibm.com>
2026-03-24 11:33:15 +0000

    rgw/posix: fix crash in radosgw-admin
    The POSIXBucket copy constructor incorrectly calls .get() on a
    on a temporary unique_ptr returned by clone(), causing immediate
    deletion of the Directory object. This leaves a dangling pointer
    that triggers a segfault during destruction.
    
    Signed-off-by: Nithya Balachandran <nithya.balachandran@ibm.com>
    (cherry picked from commit c8b1ae5d2ed63e08f1aa7e4fab7aba8f118ea928)
    Signed-off-by: Daniel Gryniewicz <dang@fprintf.net>
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/rgw/driver/posix/rgw_sal_posix.h
+++ src/rgw/driver/posix/rgw_sal_posix.h
@@ -878,7 +878,7 @@ public:
     acls(_b.acls),
     ns(_b.ns)
     {
-      dir.reset(static_cast<Directory*>(_b.dir->clone().get()));
+      dir = _b.dir->clone_dir();
     }
 
   virtual ~POSIXBucket() { }
