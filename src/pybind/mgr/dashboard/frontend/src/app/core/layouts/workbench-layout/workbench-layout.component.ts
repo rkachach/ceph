ce52759a3d976efc93748643f7ed250d50396d3c
Pedro Gonzalez Gomez <pegonzal@ibm.com>
2026-07-01 10:27:00 +0200

    mgr/dashboard/downstream: fixes after cherry-picking cephfs mirror feature
    - Changes needed on workbenchlayout because wrongfuly deleted  related items as I though telemetry realted commit was not available in downstream
    - Changes needed on notification-type.enum to add 'warning' type due to missing commit 0ab2a4e266eab4c2788c86f8bc7e571a4beb611d
    
    Resolves: ISCE-2752
    
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/layouts/workbench-layout/workbench-layout.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/layouts/workbench-layout/workbench-layout.component.ts
@@ -1,7 +1,7 @@
 import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
 
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { MultiClusterService } from '~/app/shared/api/multi-cluster.service';
 import { Permissions } from '~/app/shared/models/permissions';
@@ -13,6 +13,7 @@ import { TaskManagerService } from '~/app/shared/services/task-manager.service';
 import { TelemetryNotificationService } from '../../../shared/services/telemetry-notification.service';
 import { MotdNotificationService } from '~/app/shared/services/motd-notification.service';
 import {
+  FeatureTogglesMap,
   FeatureTogglesService
 } from '~/app/shared/services/feature-toggles.service';
 import _ from 'lodash';
@@ -35,6 +36,7 @@ export class WorkbenchLayoutComponent implements OnInit, OnDestroy {
   pageHeaderSubtitle: string | null = null;
   pageHeaderDescription: string | null = null;
   pageHeaderHidden = false;
+  enabledFeature$: Observable<FeatureTogglesMap>;
 
   @HostBinding('class') get class(): string {
     return 'top-notification-' + this.notifications.length;
