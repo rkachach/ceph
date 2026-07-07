3ae37aae3f4ae00447abf75b319e3d976d8b0dc8
Aashish Sharma <aashish@li-e9bf2ecc-2ad7-11b2-a85c-baf05c5182ab.ibm.com>
2026-07-07 13:49:41 +0530

    mgr/dashboard: carbonize Setup multi-site replication wizard
    Fixes: https://tracker.ceph.com/issues/77976
    
    Resolves: IBMCEPH-13985
    
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>
    (cherry picked from commit 2f4a86c0a3af232ad6b9c10f0dbad5e9867a8030)
    
    Conflicts:
    	src/pybind/mgr/dashboard/frontend/cypress/e2e/rgw/multisite.po.ts (adopt tests according to the new tearsheet component)
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/services/service-form/service-form.component.ts (remove methods coming from main branch not in tentacle)
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-wizard/rgw-multisite-wizard.component.html (conflicts in replacing modal with tearsheet)
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-wizard/rgw-multisite-wizard.component.spec.ts (remove ToasterModule import coming from main)
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-wizard/rgw-multisite-wizard.component.ts (conflicts in ngOninit where code is replaced in the new PR)
    	src/pybind/mgr/dashboard/frontend/src/app/shared/components/tearsheet/tearsheet.component.html (changes from this commit - Add storage carbonization tentacle not merged in tentacle yet)
    	src/pybind/mgr/dashboard/frontend/src/app/shared/components/tearsheet/tearsheet.component.spec.ts (changes from this commit - Add storage carbonization tentacle not merged in tentacle yet)
    	src/pybind/mgr/dashboard/frontend/src/app/shared/components/tearsheet/tearsheet.component.ts (changes from this commit - Add storage carbonization tentacle not merged in tentacle yet)
    (cherry picked from commit 1bdfc00f7b05f05683f5be39febc6eff12616917)
    
    Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-wizard/rgw-multisite-wizard.component.html (conflicts with environment condition for an alert panel)
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-multisite-wizard/rgw-multisite-wizard.component.ts (conflicts with position of environment variable in downstream repo)
    	src/pybind/mgr/dashboard/frontend/src/app/shared/components/tearsheet/tearsheet.component.html (conflicts with && progressPosition === 'left' condition in left influencer)
    	src/pybind/mgr/dashboard/frontend/src/app/shared/components/tearsheet/tearsheet.component.ts (conflicts with handleSubmit method in invalidStep calculation change)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/api/mgr-module.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/api/mgr-module.service.ts
@@ -97,7 +97,7 @@ export class MgrModuleService {
   updateModuleState(
     modules: string | string[],
     enabled: boolean = false,
-    table: TableComponent = null,
+    table: TableComponent | undefined = undefined,
     navigateTo: string = '',
     notificationText?: string,
     navigateByUrl?: boolean,
