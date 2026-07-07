ca687459a5249ac88c191f00e5e49e29ea17103c
Adam King <adking@redhat.com>
2026-07-07 14:47:30 -0400

    mgr/cephadm: Revert "mgr/cephadm: wipe auth rotation service keyrings at the end of upgrade"
    This reverts commit a615cc76ffbd8781c077d1174e8e666295b7668e.
    
    It was decided it was best to not have the upgrade process automatically
    do this.
    
    Resolves: IBMCEPH-15232
    Change-Id: ab1f8e7d-4a4a-4dee-9b7a-5695b7bc0413
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 123e10eff29923549f360f520cea15f81e125b77)
    (cherry picked from commit cc4d892a1be24218e0d9d9fc6ca551d21543b623)

--- src/pybind/mgr/cephadm/upgrade.py
+++ src/pybind/mgr/cephadm/upgrade.py
@@ -2519,10 +2519,7 @@ class CephadmUpgrade:
         # Restore OSD flags before we clear the upgrade state.
         self._restore_osd_flags_after_upgrade()
 
-        logger.info(
-            'Upgrade: checking if all mon, mgr, OSD, mds daemons upgraded before '
-            'changing service cipher and clearing rotating keys'
-        )
+        logger.info('Upgrade: checking if all mon, mgr, OSD, mds daemons upgraded before changing service cipher')
         daemons_to_check = (
             self.mgr.cache.get_daemons_by_service('mon')
             + self.mgr.cache.get_daemons_by_service('mgr')
@@ -2539,12 +2536,8 @@ class CephadmUpgrade:
                 'name': 'auth_service_cipher',
                 'value': SERVICE_CIPHER,
             })
-            # wipe the rotating service keys to have them be re-made on the new cipher
-            ret, image, err = self.mgr.check_mon_command({
-                'prefix': 'auth wipe-rotating-service-keys',
-            })
         else:
-            logger.info('Found mon/mgr/OSD/mds daemons still needing upgrade. Service cipher not set. Rotating keys not cleared')
+            logger.info('Found mon/mgr/OSD/mds daemons still needing upgrade. Service cipher not set')
 
         if self.upgrade_state.rotated_osd_mds_keyrings:
             self._clear_rotated_daemon_entry()
