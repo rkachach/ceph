2f27fc9bfac333b0210627fb35c78632cce35b00
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2026-07-09 16:23:54 +0530

    cephadm/nfs: add configurable CephFS client logging for Ganesha
    When enable_cephfs_client_log is set, cephadm mounts the host log
    directory into the Ganesha container at /var/log/ceph and adds a
    [client] section to the daemon ceph.conf with a configurable debug
    level and a fixed log file path of /var/log/ceph/$name.$pid.log.
    
    New NFSServiceSpec fields:
    - enable_cephfs_client_log: enable logging and log dir mount
    - cephfs_client_log_level: client debug level (default: 10)
    - cephfs_client_log_dir: host mount source (default:
      /var/log/ceph/<cluster-fsid>)
    
    Fixes: https://tracker.ceph.com/issues/78087
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>
    
    Resolves: IBMCEPH-8697
    
     Conflicts:
    	src/cephadm/cephadmlib/daemons/nfs.py
    	src/pybind/mgr/cephadm/services/nfs.py
    	src/python-common/ceph/deployment/service_spec.py
    	src/python-common/ceph/tests/test_service_spec.py

--- src/cephadm/cephadmlib/daemons/nfs.py
+++ src/cephadm/cephadmlib/daemons/nfs.py
@@ -4,7 +4,7 @@ import re
 from typing import Dict, List, Optional, Tuple, Union
 
 from ..call_wrappers import call, CallVerbosity
-from ..constants import DEFAULT_IMAGE, CEPH_DEFAULT_CONF
+from ..constants import DEFAULT_IMAGE, CEPH_DEFAULT_CONF, LOG_DIR_MODE
 from ..container_daemon_form import ContainerDaemonForm, daemon_to_container
 from ..container_types import CephContainer, extract_uid_gid
 from ..context import CephadmContext
@@ -61,6 +61,12 @@ class NFSGanesha(ContainerDaemonForm):
         self.files = dict_get(config_json, 'files', {})
         self.rgw = dict_get(config_json, 'rgw', {})
         self.enable_rdma = dict_get(config_json, 'enable_rdma', False)
+        self.enable_cephfs_client_log = dict_get(
+            config_json, 'enable_cephfs_client_log', False
+        )
+        self.cephfs_client_log_dir = dict_get(
+            config_json, 'cephfs_client_log_dir', None
+        )
 
         # validate the supplied args
         self.validate()
@@ -95,11 +101,19 @@ class NFSGanesha(ContainerDaemonForm):
             ] = '/var/lib/ceph/radosgw/%s-%s/keyring:z' % (cluster, rgw_user)
         return mounts
 
+    def _get_host_log_dir(self, ctx: CephadmContext) -> str:
+        if self.cephfs_client_log_dir:
+            return self.cephfs_client_log_dir
+        return os.path.join(ctx.log_dir, self.identity.fsid)
+
     def customize_container_mounts(
         self, ctx: CephadmContext, mounts: Dict[str, str]
     ) -> None:
         data_dir = self.identity.data_dir(ctx.data_dir)
         mounts.update(self._get_container_mounts(data_dir))
+        if self.enable_cephfs_client_log:
+            host_log_dir = self._get_host_log_dir(ctx)
+            mounts[host_log_dir] = '/var/log/ceph:z'
 
     @staticmethod
     def get_container_envs():
@@ -205,6 +219,11 @@ class NFSGanesha(ContainerDaemonForm):
         populate_files(config_dir, config_files, uid, gid)
         populate_files(kmip_dir, kmip_files, uid, gid)
         populate_files(tls_dir, tls_files, uid, gid)
+
+        if self.enable_cephfs_client_log:
+            host_log_dir = self._get_host_log_dir(self.ctx)
+            if not os.path.exists(host_log_dir):
+                makedirs(host_log_dir, uid, gid, LOG_DIR_MODE)
 
         # write the RGW keyring
         if self.rgw:
