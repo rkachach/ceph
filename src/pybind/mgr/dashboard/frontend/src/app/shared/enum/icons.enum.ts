6f6dd3bf040229447b4c66f00c1aef22be00864d
Afreen Misbah <afreen@ibm.com>
2026-07-28 01:55:03 +0530

    mgr/dashboard/downstream: Fix build errors
    Resolves: IBMCEPH-16031
    
    Signed-off-by: Afreen Misbah <afreen@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/enum/icons.enum.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/enum/icons.enum.ts
@@ -179,7 +179,7 @@ export const ICON_TYPE = {
   warningAlt: 'warning--alt',
   checkMarkOutline: 'checkmark--outline',
   arrowRight: 'arrow--right',
-  arrowUpRight: ' arrow--up-right',
+  arrowUpRight: 'arrow--up-right',
   inProgress: 'in-progress',
   arrowDown: 'arrow--down',
   warning: 'warning',
