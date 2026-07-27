890ec59c73a6d2e7bb37747827ee17fe644a82e9
Adam C. Emerson <aemerson@redhat.com>
2026-07-27 10:56:01 -0400

    rgw: Add `uppercase_dash_transform` as needed by Sigv4 fix
    Resolves: IBMCEPH-15857
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1978,6 +1978,39 @@ extern bool match_policy(const std::string& pattern, const std::string& input,
 extern std::string camelcase_dash_http_attr(const std::string& orig, bool convert2dash = true);
 extern std::string lowercase_dash_http_attr(const std::string& orig, bool bidirection = false);
 
+/*
+ * Converts lower to upper and dashes to underscores
+ *
+ * 'this-kind-of-string' to 'THIS_KIND_OF_STRING'
+ *
+ * \param[in] in Range to transform
+ * \param[out] out Output iterator
+ * \param[in] bidirectional Transform '_' to '-'
+ *
+ * \return A structure of input and output iterators, as with std::transform.
+ */
+inline auto
+uppercase_dash_transform(
+    std::ranges::input_range auto&& in,
+    std::output_iterator<char> auto out,
+    bool bidirectional = false)
+{
+  return std::ranges::transform(
+      std::forward<decltype(in)>(in), out, [bidirectional](char c) -> char {
+        switch (c) {
+        case '-':
+          return '_';
+        case '_':
+          if (bidirectional)
+            return '-';
+          else
+            return toupper(static_cast<unsigned char>(c));
+        default:
+          return toupper(static_cast<unsigned char>(c));
+        }
+      });
+}
+
 void rgw_setup_saved_curl_handles();
 void rgw_release_all_curl_handles();
 
