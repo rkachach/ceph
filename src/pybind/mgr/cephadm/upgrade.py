a1e6018c109e64d5227220502adf5c0ff01d814d
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2026-07-28 22:50:49 +0530

    mgr/cephadm: set filesystems joinable immediately after MDS daemons are upgraded
    Previously, after upgrading MDS daemons, the code would return control
    to the serve loop and rely on the next iteration to detect (via cephadm
    cache) that no more MDS daemons need upgrading, then call
    _complete_mds_upgrade(). This added an unnecessary serve loop iteration
    delay before filesystems were set joinable again.
    
    Now, immediately after upgrading MDS daemons, _verify_all_mds_upgraded()
    queries 'ceph versions' to confirm all MDS daemons report the target
    version. If verified, _complete_mds_upgrade() is called right away
    before returning to the serve loop, reducing the window during which
    filesystems remain not-joinable
    
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>
    Resolves: IBMCEPH-17180

--- src/pybind/mgr/cephadm/upgrade.py
+++ src/pybind/mgr/cephadm/upgrade.py
@@ -215,6 +215,7 @@ class UpgradeState:
                  prior_autoscale: Optional[bool] = True,
                  crush_bucket_type: Optional[str] = None,
                  crush_bucket_name: Optional[str] = None,
+                 mds_upgrade_completed: Optional[bool] = False,
                  ):
 
         self._target_name: str = target_name  # Use CephadmUpgrade.target_image instead.
@@ -239,6 +240,7 @@ class UpgradeState:
         self.prior_autoscale = prior_autoscale
         self.crush_bucket_type = crush_bucket_type
         self.crush_bucket_name = crush_bucket_name
+        self.mds_upgrade_completed = mds_upgrade_completed
 
     def to_json(self) -> dict:
         return {
@@ -262,6 +264,7 @@ class UpgradeState:
             'prior_autoscale': self.prior_autoscale,
             'crush_bucket_type': self.crush_bucket_type,
             'crush_bucket_name': self.crush_bucket_name,
+            'mds_upgrade_completed': self.mds_upgrade_completed,
         }
 
     @classmethod
@@ -2169,6 +2172,22 @@ class CephadmUpgrade:
                 else:
                     raise
 
+    def _verify_all_mds_upgraded(self, target_version: str) -> bool:
+        ret, out_ver, err = self.mgr.check_mon_command({
+            'prefix': 'versions',
+        })
+        j = json.loads(out_ver)
+        mds_versions = j.get('mds', {})
+        all_upgraded = True
+        for version, count in mds_versions.items():
+            short_version = version.split(' ')[2]
+            if short_version != target_version:
+                logger.warning(
+                    'Upgrade: %d mds daemon(s) are %s != target %s' %
+                    (count, short_version, target_version))
+                all_upgraded = False
+        return all_upgraded
+
     def _complete_mds_upgrade(self) -> None:
         assert self.upgrade_state is not None
         if self.upgrade_state.fail_fs:
@@ -2411,6 +2430,17 @@ class CephadmUpgrade:
                 return
             self._upgrade_daemons(to_upgrade, target_image, target_digests)
             if to_upgrade:
+                if daemon_type == 'mds' and self.upgrade_state.fail_fs:
+                    if self._verify_all_mds_upgraded(target_version):
+                        logger.info('Upgrade: All MDS daemons upgraded to %s, '
+                                    'setting filesystems joinable' % target_version)
+                        self._complete_mds_upgrade()
+                        self.upgrade_state.mds_upgrade_completed = True
+                        self._save_upgrade_state()
+                    else:
+                        logger.info('Upgrade: Waiting for all MDS daemons to report '
+                                    'target version %s before setting filesystems '
+                                    'joinable' % target_version)
                 return
 
             self._handle_need_upgrade_self(need_upgrade_self, daemon_type == 'mgr')
@@ -2450,7 +2480,7 @@ class CephadmUpgrade:
                 self._complete_osd_upgrade(target_major, target_major_name)
 
             # complete mds upgrade?
-            if daemon_type == 'mds':
+            if daemon_type == 'mds' and not self.upgrade_state.mds_upgrade_completed:
                 self._complete_mds_upgrade()
 
             # Make sure all metadata is up to date before saying we are done upgrading this daemon type
