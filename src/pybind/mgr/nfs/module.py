fdd03dad78672776ea9cc4f4389f67b7c7aa8d5b
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2026-07-30 16:36:31 +0530

    mgr/nfs: add nfs cluster rotate-key command
    Add `ceph nfs cluster rotate-key` to rotate NFS auth keys for a
    cluster. Cluster id is required, when entities are omitted, rotate all
    cluster keys (daemon and CephFS export). Refresh exports after export-key
    rotation, and redeploy the NFS service after daemon-key rotation.
    Optional --key-type is passed through to auth rotate.
    
    Fixes: https://tracker.ceph.com/issues/78876
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>
    Resolves: IBMCEPH-17308

--- src/pybind/mgr/nfs/module.py
+++ src/pybind/mgr/nfs/module.py
@@ -217,7 +217,20 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
         """Update configuration in EXPORT DEFAULT block (global fallback for all exports)."""
         return self.export_mgr.set_export_default(cluster_id, delegations.value)
 
-    @CLICommand('nfs cluster create', perm='rw')
+    @NFSCLICommand('nfs cluster rotate-key', perm='rw')
+    @object_format.Responder()
+    def _cmd_nfs_cluster_rotate_key(self,
+                                    cluster_id: str,
+                                    entity: Optional[List[str]] = None,
+                                    key_type: Optional[str] = None) -> Dict[str, Any]:
+        """Rotate NFS cluster/daemon and export auth keys; redeploy service if daemon keys change"""
+        return self.nfs.rotate_keys(
+            cluster_id=cluster_id,
+            entity=entity,
+            key_type=key_type
+        )
+
+    @NFSCLICommand('nfs cluster create', perm='rw')
     @object_format.EmptyResponder()
     def _cmd_nfs_cluster_create(self,
                                 cluster_id: str,
