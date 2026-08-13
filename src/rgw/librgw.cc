ceph/ceph-ci

rgw: add support for SMB frontend daemon
Add FrontendType enum to AppMain to support multiple frontend daemon
types (HTTP, NFS, SMB), enabling the rgw-smb daemon to follow the
same initialization flow as rgw-nfs.

The SMB daemon now has complete infrastructure with configuration
framework, thread control, and initialization flow identical to NFS.

Fixes: https://tracker.ceph.com/issues/78631

Signed-off-by: Shweta Sodani <ssodani@redhat.com>

src/rgw/librgw.cc
@@ -65,7 +65,11 @@ int librgw_create(librgw_t* rgw, int argc, char **argv)
       for (const auto& elt : spl_args) {
 	args.push_back(elt.c_str());
       }
-      rc = rgwlib.init(args);
+      if (strcmp(argv[0], "vfs_ceph_rgw") == 0) {
+        rc = rgwlib.init(args, FrontendType::SMB);
+      } else {
+        rc = rgwlib.init(args, FrontendType::NFS);
+      }
     }
   }
 

rkachach/ceph

Add InstanceType enum to AppMain to support multiple protocol
types (HTTP, NFS, SMB), enabling the rgw-smb process to follow the
Resolves: IBMCEPH-17059
Signed-off-by: Shweta Sodani <Shweta.Sodani@ibm.com>
(cherry picked from commit 7195c53001921b87c419bd9f696e51d211339017)
Conflicts:
	src/rgw/rgw_main.h

src/rgw/librgw.cc
@@ -65,7 +65,11 @@ int librgw_create(librgw_t* rgw, int argc, char **argv)
       for (const auto& elt : spl_args) {
 	args.push_back(elt.c_str());
       }
-      rc = rgwlib.init(args);
+      if (strcmp(argv[0], "vfs_ceph_rgw") == 0) {
+        rc = rgwlib.init(args, ProtocolType::SMB);
+      } else {
+        rc = rgwlib.init(args, ProtocolType::NFS);
+      }
     }
   }
 
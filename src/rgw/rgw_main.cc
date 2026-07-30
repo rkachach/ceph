7255a5c5300dbdf07e90ad803605f078d6ce6003
Shweta Sodani <Shweta.Sodani@ibm.com>
2026-07-30 16:41:38 +0530

    rgw: add support for SMB frontend daemon
    Add InstanceType enum to AppMain to support multiple protocol
    types (HTTP, NFS, SMB), enabling the rgw-smb process to follow the
    same initialization flow as rgw-nfs.
    
    The SMB daemon now has complete infrastructure with configuration
    framework, thread control, and initialization flow identical to NFS.
    
    Fixes: https://tracker.ceph.com/issues/78631
    Resolves: IBMCEPH-17059
    
    Signed-off-by: Shweta Sodani <ssodani@redhat.com>
    Signed-off-by: Shweta Sodani <Shweta.Sodani@ibm.com>
    (cherry picked from commit 7195c53001921b87c419bd9f696e51d211339017)
    
    Conflicts:
    	src/rgw/rgw_main.h

--- src/rgw/rgw_main.cc
+++ src/rgw/rgw_main.cc
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
 
   MemoryKeyringSecret::initialize_process_keyring();
 
-  main.init_frontends1(false /* nfs */);
+  main.init_frontends1(rgw::InstanceType::Daemon, rgw::ProtocolType::HTTP_S3);
   main.init_numa();
 
   if (g_conf()->daemonize) {
